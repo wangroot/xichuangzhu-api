a3e1516d196355e84cd7a78574a3c7b2df86b67f
hustlzp <hustlzp@qq.com>
2014-09-13 23:55:45 +0800

    use mysq instead of node-mysql, and return work update info from /update

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'xcz'
 });
 
+connection.connect();
+
+// 获取加精作品的更新信息
+app.get('/update', function (req, res) {
+  connection.query('SELECT * from work WHERE highlight = 1', function (err, rows) {
+    if (err) throw err;
+
+    var update_info = [];
+
+    rows.forEach(function (row) {
+      update_info.push({
+        'id': row.id,
+        'update_at': row.updated_at
+      });
+    });
+
+    res.json(update_info);
+  });
+});
+
+// Error handle
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something broke!');
